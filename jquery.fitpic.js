948766b665045565ba798b5c2bc18b9b57fcc722
yckart <mail@yckart.com>
2013-04-02 07:56:55 +0200

    fixed plugin json

--- jquery.fitpic.js
+++ jquery.fitpic.js
@@ -1,30 +1,51 @@
 /*!
- * jquery.fitpic.js 0.0.2 - https://github.com/yckart/jquery.fitpic.js
+ * jquery.fitpic.js 0.0.3 - https://github.com/yckart/jquery.fitpic.js
  * Stretch images perfect.
  *
  * Copyright (c) 2013 Yannick Albert (http://yckart.com)
  * Licensed under the MIT license (http://www.opensource.org/licenses/mit-license.php).
- * 2013/02/09
+ * 2013/04/02
 */
-(function($, window){
-    $.fn.fitPic = function(url) {
-        var winW = $(window).width(),
-            winH = window.innerHeight || $(window).height(); // `innerHeight` for mobile safari
+(function ($, window) {
 
-        return this.each(function() {
-            if(url) this.src = url;
-            var elem = $(this),
-                elemW = elem.width(),
-                elemH = elem.height(),
+    var win = $(window);
 
-                imgW = (elemW * winH) / elemH,
-                imgH = (elemH * winW) / elemW,
+    // Compute new dimensions for image
+    function fitPic(elem) {
+        var winW = win.width(),
+            winH = window.innerHeight || win.height(), // `innerHeight` for mobile safari
 
-                newW = imgH < winH ? imgW : winW,
-                newH = imgH < winH ? winH : imgH;
+            elemW = elem.width,
+            elemH = elem.height,
 
-            this.style.width = newW + "px";
-            this.style.height = newH + "px";
+            imgW = (elemW * winH) / elemH,
+            imgH = (elemH * winW) / elemW,
+
+            // Check the width and height
+            ratioW = imgH < winH ? imgW : winW,
+            ratioH = imgH < winH ? winH : imgH,
+
+            // Position the image
+            left = (ratioW - winW) / 2,
+            top = (ratioH - winH) / 2;
+
+        elem.style.width = ratioW + "px";
+        elem.style.height = ratioH + "px";
+        elem.style.marginTop = -top + "px";
+        elem.style.marginLeft = -left + "px";
+    }
+
+    $.fn.fitPic = function () {
+        return this.each(function () {
+            var elem = this,
+                timeout;
+
+            fitPic(elem);
+            win.resize(function () {
+                clearTimeout(timeout);
+                timeout = setTimeout(fitPic, 100, elem);
+            });
         });
     };
+
 }(jQuery, window));
